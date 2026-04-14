d35295fce04e3d7a040316ed092f26843ce2afb0
BasiratHamayoon <basirathumayun@gmail.com>
2026-04-14 20:19:31 +0500

    adding build command

---
+++ app/(dashboard)/consulting/consultants/page.jsx
@@ -0,0 +1,3 @@
+export default function ConsultantsPage() {
+    return <div>Consultants - Coming Soon</div>;
+  }
---
+++ app/(dashboard)/consulting/page.jsx
@@ -0,0 +1,3 @@
+export default function ConsultaningPage() {
+    return <div>Consultants - Coming Soon</div>;
+  }
