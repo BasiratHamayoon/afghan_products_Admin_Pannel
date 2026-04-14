b9f437dcb6015e9cbefcdf877cf01afb9d4eae3a
BasiratHamayoon <basirathumayun@gmail.com>
2026-04-14 19:38:05 +0500

    Delete all the files

--- hooks/useAuth.js
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useSelector } from "react-redux";
-
-export function useAuth() {
-  const { user, isAuthenticated, isLoading, token } = useSelector((state) => state.auth);
-  return { user, isAuthenticated, isLoading, token };
-}
